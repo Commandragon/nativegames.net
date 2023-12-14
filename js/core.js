e0910c7acde6a84447f0349eff402e9435589bc8
Commandragon <119017455+Commandragon@users.noreply.github.com>
2023-12-13 18:02:51 -0600

    Update core.js

--- js/core.js
+++ js/core.js
@@ -36,37 +36,7 @@ rocket.async = true;
 rocket.src = "/js/rocket-loader.min.js";
 document.body.appendChild(rocket);
 
-// Collect Tab Cloak data from local storage
-var tab = localStorage.getItem("tab");
-if (tab) {
-  try {
-    // Parse the data, it is in JSON
-    var tabData = JSON.parse(tab);
-  } catch {
-    var tabData = {};
-  }
-} else {
-  var tabData = {};
-}
 
-// Set the Tab title if the Tab cloak data is there
-if (tabData.title) {
-  document.title = tabData.title;
-}
-
-// Set the Tab icon if the Tab cloak data is there
-if (tabData.icon) {
-  document.querySelector('link[rel="icon"]').href = tabData.icon;
-}
-document.addEventListener("DOMContentLoaded", function () {
-  const themeSelect = document.getElementById("themeSelect");
-
-  // Check the saved theme from local storage
-  const savedTheme = localStorage.getItem("theme");
-  if (savedTheme) {
-    applyTheme(savedTheme);
-    themeSelect.value = savedTheme;
-  }
 
   // Apply selected theme
   themeSelect.addEventListener("change", function () {
